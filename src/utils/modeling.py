2d8c27bcccd4bbf1c6cc111472679968f2d6a07b
Leandro <jose.leandro2002@gmail.com>
2023-11-05 22:34:04 +0000

    added randomforest and neural net GridSearch

--- src/utils/modeling.py
+++ src/utils/modeling.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier, export_graphviz, plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 import seaborn as sb
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
@@ -49,10 +51,10 @@ def runModel(df, model, test_year=10):
     train_df = df[df['year'] < test_year]
     test_df = df[df['year'] == test_year]
 
-    X_train = train_df.drop(columns=['playoff', 'confID'])
+    X_train = train_df.drop(columns=['playoff'] + (['confID'] if 'confID' in train_df.columns else []))
     y_train = train_df['playoff']
 
-    X_test = test_df.drop(columns=['playoff', 'confID'])
+    X_test = test_df.drop(columns=['playoff'] + (['confID'] if 'confID' in test_df.columns else []))
     y_test = test_df['playoff']
 
     model.fit(X_train, y_train)
@@ -101,10 +103,10 @@ def treeGridSearch(df, test_year=10):
     train_df = df[df['year'] < test_year]
     test_df = df[df['year'] == test_year]
 
-    X_train = train_df.drop(columns=['playoff', 'confID'])
+    X_train = train_df.drop(columns=['playoff'] + (['confID'] if 'confID' in train_df.columns else []))
     y_train = train_df['playoff']
 
-    X_test = test_df.drop(columns=['playoff', 'confID'])
+    X_test = test_df.drop(columns=['playoff'] + (['confID'] if 'confID' in test_df.columns else []))
     y_test = test_df['playoff']
 
     # grid search trees hyperparamet
@@ -116,6 +118,58 @@ def treeGridSearch(df, test_year=10):
         'max_features': ['auto', 'sqrt', 'log2']
     }
     model = DecisionTreeClassifier()
+    grid = GridSearchCV(model, param_grid, cv=5, verbose=1, n_jobs=-1)
+    grid.fit(X_train, y_train)
+    print(grid.best_params_)
+    print(grid.best_score_)
+    print(grid.best_estimator_)
+    return grid
+
+def RandomForest_GridSearch(df, test_year=10):
+    """
+        Perform a grid search for a Random Forest model
+    """
+    train_df = df[df['year'] < test_year]
+
+    X_train = train_df.drop(columns=['playoff'] + (['confID'] if 'confID' in train_df.columns else []))
+    y_train = train_df['playoff']
+
+    # grid search Random Forest's hyperparameters
+    param_grid = {
+        'n_estimators': [5*x for x in range(1,10)],
+        'criterion': ['gini', 'entropy', 'log_loss'],
+        'max_depth': [None] + list(range(1, 10)),
+        'max_features': [None, 'sqrt', 'log2'],
+        'min_samples_split': range(2, 10),
+        # 'min_samples_leaf': range(1, 10),
+    }
+    model = RandomForestClassifier()
+    grid = GridSearchCV(model, param_grid, cv=5, verbose=1, n_jobs=-1)
+    grid.fit(X_train, y_train)
+    print(grid.best_params_)
+    print(grid.best_score_)
+    print(grid.best_estimator_)
+    return grid
+
+def NeuralNet_GridSearch(df, test_year=10):
+    """
+        Perform a grid search for a Neural Net model
+    """
+    train_df = df[df['year'] < test_year]
+
+    X_train = train_df.drop(columns=['playoff'] + (['confID'] if 'confID' in train_df.columns else []))
+    y_train = train_df['playoff']
+
+    # grid search Random Forest's hyperparameters
+    param_grid = {
+        'hidden_layer_sizes': [(100, 100, 100), (100, 100), (100, 100, 100, 100)],
+        'activation': ['tanh', 'relu'],
+        'solver': ['lbfgs', 'sgd', 'adam'],
+        'alpha': [0.0001, 0.05],
+        'learning_rate': ['constant', 'adaptive'],
+        'max_iter': [5000]
+    }
+    model = MLPClassifier(random_state=42)
     grid = GridSearchCV(model, param_grid, cv=5, verbose=1, n_jobs=-1)
     grid.fit(X_train, y_train)
     print(grid.best_params_)
